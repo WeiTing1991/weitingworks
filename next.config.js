5ed47545fed28d3639041f7c53bc90d787245ae9
WeiTing1991 <>
2025-03-23 03:00:19 +0100

    clean up

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 // const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
 	reactStrictMode: true,
-	images: {
-		unoptimized: true, // Disable default image optimization
-	},
-	// assetPrefix: isProd ? '/weitingworks/' : '',
-	basePath: "/weitingworks", // Matches your GitHub Pages URL (https://WeiTing1991.github.io/weitingworks/)
-	output: "standalone",
-	pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+	// images: {
+	// 	unoptimized: true, // Disable default image optimization
+	// },
+	// // assetPrefix: isProd ? '/weitingworks/' : '',
+	// basePath: "/weitingworks", // Matches your GitHub Pages URL (https://WeiTing1991.github.io/weitingworks/)
+	// output: "standalone",
+	// pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
 	// distDir: "build",
 };
 
